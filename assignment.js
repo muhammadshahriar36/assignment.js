e1e404c748e7a5aa3ec0eb67a905d03fb47395d5
muhammadshahriar36 <muhammadshahriar36@gmail.com>
2021-01-22 22:41:45 +0600

    js projects

---
+++ assignment.js
@@ -0,0 +1,50 @@
+function kilometerToMeter(kilometer){
+    var meter = kilometer * 1000;
+    return meter;
+}
+
+
+function budgetCalculator(watch, mobile, laptop){
+    watch = watch*50;
+    mobile = mobile*100;
+    laptop = laptop*500;
+    var taka = watch + mobile + laptop;
+    return taka;
+}
+
+
+function hotelCost(day){
+    var taka = 0;
+    if(day<=10){
+        taka = day*100;
+    }
+    else if(day<=20){
+        var firstPart = 10*100;
+        var remaining = day - 10;
+        var secondPart = remaining*80;
+        taka = firstPart + secondPart;
+    }
+    else{
+        var firstPart = 10*100;
+        var secondPart = 10*80;
+        var remaining = day - 20;
+        var thirdPart = remaining*50;
+        taka = firstPart + secondPart + thirdPart;
+    }
+    return taka;
+}
+
+
+function megaFriend(names){
+    var max = names[0];
+    for(var i = 0; i < names.length; i++){
+        var element = names[i];
+        if(element.length > max.length){
+            max = element;
+        }
+    }
+    return max;
+}
+
+
+
